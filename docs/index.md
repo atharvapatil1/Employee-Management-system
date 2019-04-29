4cd4c8d8fd6419623d8f34f2939ab8a94792979a
Atharva Patil <44890114+atharvapatil1@users.noreply.github.com>
2019-04-29 09:06:12 +0530

    Add files via upload

---
+++ docs/index.md
@@ -0,0 +1,172 @@
+<link rel="shortcut icon" type="image/x-icon" href="{{ site.url }}/employee-management-system/favicon.ico">
+
+# Welcome
+
+Welcome to the employee management system! This documentation is divided into many different parts. We recommended you get started with installation. This program is made in java with the JFrame GUI package. Feel free to view the code at github.com/ningsongshen/employee-management-system. It's open source!
+
+
+1. [Installation](#installation)
+    1. [Developers](#subinstallation)
+    2. [Users](#sub2installation)
+2. [Homepage](#homepage)
+3. [Adding](#adding)
+4. [Searching](#searching)
+5. [Removing](#removing)
+6. [Editing](#editing)
+7. [Viewing](#viewing)
+8. [Saving](#saving)
+    1. [The Archive Format](#subsaving)
+9. [Opening](#opening)
+10. [Updating](#updating)
+11. [Customizing](#customizing)
+12. [Mulitple Windows](#multiple-windows)
+13. [Exiting](#exiting)
+
+
+
+# Installation <a name="installation"></a>
+
+### Developers: <a name="subinstallation"></a>
+
+1. Install Java.
+2. Install an IDE (Eclipse or Netbeans)
+3. Open the project called 'employee-management-system'.
+4. In the file directory of the IDE, navigate to  and open:
+	employee-management-system >> src >> <default package> >> MainFrame.java
+5. Press the 'run' button.
+
+### Users: <a name="sub2installation"></a>
+
+Coming soon
+
+# The Homepage <a name="homepage"></a>
+
+The homepage displays the name of the program. It also has a text area below with information that you, the user, can add about your company. Underneath, there are statistics that are automatically generated from all the employee information available. 
+
+These areas of the homepage may be blank or have zero values if employees have not been added or a previous file has not been loaded yet. Try adding an employee or a file to see the results.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/home.png)
+
+# Adding <a name="adding"></a>
+
+To add an employee to the system, click on the 'Add' tab on the left hand side of the page. Many input fields will appear, and information is entered into these fields. You must specify whether this employee is full time or part time, and the available fields will update accordingly. 
+
+Employee Number - this is the reference number for the employee and cannot be changed
+
+First Name - this is the first (and possibly middle) name of the employee
+
+Last name - this is the last name/family name of the employee
+
+Sex - this is the sex of the employee. Use the drop down to select one. There is an option for non-disclosure.
+
+Location - this is the main work location/office that the employee works at. This can be changed if needed.
+
+Deduct Rate - this is the total deductions for the salary in decimals. (to get percentage, multiply by 100%)
+
+After this, you will need to decide whether or not this employee if full time or part time. Don't worry, this is not a permanent decision and can be changed later. Fill in the required fields for the type of employee that this person will be.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/add.png)
+
+Most fields are required. An error message will appear if you leave a field blank or input an invalid entry:
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/adderror.png)
+
+Once you click submit, a confirmation dialog will open:
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/addconfirm.png)
+
+Please review the information and ensure that it is accurate. Then, click ok and the employee will be added to the system.
+
+Note: the employee number cannot be edited after creation.
+
+
+# Searching <a name="searching"></a>
+
+To search for an employee in the system, click on the 'Search' tab on the left hand side of the page. There will be a text input field for the employee number, and a search bar underneath. After entering the employee number you wish to search for, click search. The text area underneath will update with the employee's information. 
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/search1.png)
+
+If the employee does not exist, it will let you know that. Searching by employee names is not available yet.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/search2.png)
+
+The search page also has the functions to remove and edit the employee, see below.
+
+# Removing <a name="removing"></a>
+
+Removing an employee is the second step in searching for the employee to remove, and so this feature is under the 'search' tab. After an employee's information is retrieved, pressing the 'Delete' button will pop up a confirmation dialog, and clicking yes will remove the employee. Clicking no will cancel the operation.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/remove.png)
+
+# Editing <a name="editing"></a>
+
+Editing the employee is the second step in searching for the employee to edit, and so this feature is under the 'search' tab. After and employee's information is retrieved, pressing the 'edit' button will pop up an editing dialog. This dialog looks nearly identical to the add employee page, and the fields that are available for editing can be modified. The type of employee and employee number cannot be changed, so if this information is to be changed, treat the employee as a new employee and add it to the system.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/edit.png)
+
+# Viewing <a name="viewing"></a>
+
+Viewing all employees can be done by clicking the 'view all' tab on the left hand side of the page. There will be a chart that is empty. To see the employees currently in the system, click the 'refresh' button at the bottom of the page. This action will update the table to reflect the current employees in the system. Make sure you click the 'refresh' button every time you access this page to ensure you have the latest information.
+
+If when you click refresh there is nothing displayed, the likely issue is that there are no employees in the system or a file has not been loaded. Try adding some employees or loading in a previously used file. 
+
+If when you click refresh there is no change, you probably made no changes to any employees. Try making a change.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/viewall.png)
+
+# Saving <a name="saving"></a>
+
+To save the entire system to a specific location, press 'Save As'. A file chooser dialog will pop up, and ask for a location and a name. The program will then save the file at your specified location and name. To save the file again, choose the same file name and location to save. The 'save' function will be implemented in the near future. Make sure to save before you exit the program, or you risk the loss of all your data. To ensure data security and stability, please DO NOT edit the file by opening it in another program. There is a risk of changing the file format, and this could cause severe data loss.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/saveas.png)
+
+Make sure you continuously save your file as you work, to ensure that there is no data loss. The developers are not responsible for any damages caused by using this program.
+
+### Developers: <a name="subsaving"></a>
+
+When a database is saved as, the hashtable data is stored in a text file with the suffix -[DO NOT EDIT].txt
+The information is in the following format:
+
+	employeeType/employeeNumber/firstName/lastName/sex/workLocation/deductionRate/yearlySalary/hourlyWage/hourPerWeek/weeksPerYear
+
+Each attribute is separated with a / delimeter and each employee is stored on a new line.
+Note: the format of the information stored is generic for all employee types. any information that is not associated with a specific employee type will be saved as null (i.e. for a line that stores a full time employee, the attributes for hourlyWage, hoursPerWeek, and weeksPerYear will be saved as null).
+
+Opening this file can help with debugging, and finding out what went wrong. You can also manually change the data in the file, but be careful to not modify the structure.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/archive.png)
+
+# Opening <a name="opening"></a>
+
+Click on File > Open. This will pop up a confirmation dialog warning of the deletion of the current information is the new system is opened. Make sure to save the current data before opening a new system. A file chooser will pop up once you acknowledge the warning, and choose the file with the name that you saved last time. It will have a [DO NOT EDIT] appended to it. This action will open your file and load it into the system. Then, you are ready to do everything else like before: add, save, modify, etc.
+
+Please make sure the file is of the correct type, and is compatible with the employee management system. This means that it must have been created with the system, and should have the [DO NOT EDIT] appended to the end of it.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/open.png)
+
+The home tab will be updates to display the file path of your archive file:
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/openedhome.png)
+
+# Updating <a name="updating"></a>
+
+To update the program to its latest version, click on Help > Check for Updates. This action will automatically update the system if one is found. If no updates are found, you will be informed of that. The updates button will only check once every instance. To check for updates again, please restart the program.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/update.png)
+
+# Customizing <a name="customizing"></a>
+
+The program has a unique and special feature that will allow you to customize the program. Adding your own customizations means adding your favourite colours, company colours, or adding colours that are easy on the eye. Currently, only the background colour customizer is available, and others will be added in future updates.
+
+![Screenshot](https://ningsongshen.github.io/employee-management-system/preferences.png)
+
+Currently, dark colours may hide text and that will hinder the operation of the program. In future updates, there will be an option for light colour text. For now, choose a lighter coloured background.
+
+# Multiple Windows <a name="multiple-windows"></a>
+
+To open another instance of the program, press File > New. This is a completely new instance, and is unaffected by the original instance. Use it to work on multiple files concurrently. In fact, there are virtually unlimited instances that can be opened. To test it, try holding down Ctrl+N for 10 seconds.
+
+# Exiting <a name="exiting"></a>
+
+To exit a window, click the X button on the top right. If you have multiple windows open and wish to exit all of them, go to File > Exit All Windows or press ctrl + shift + E. Please ensure that all your work is saved before you close, as it will not confirm your exit. This is to ensure a speedy and efficient exit.
+
---
+++ src/MyHashTable.java
@@ -0,0 +1,279 @@
+
+import java.util.ArrayList;
+
+public class MyHashTable {
+
+    // ATTRIBUTES
+    // buckets is an array of ArrayList. Each item in an ArrayList is an
+    // EmployeeInfo object.
+    private ArrayList<EmployeeInfo>[] buckets;
+    private int employeeCount;
+    private double yearlyPayroll;
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public double getYearlyPayroll() {
+        return yearlyPayroll;
+    }
+    //public void setYearlyPayroll(double yearlyPayrollChange) {
+        //yearlyPayroll += yearlyPayrollChange;
+    //}
+    // CONSTRUCTOR
+    public MyHashTable(int howManyBuckets) {
+        // Construct the hash table (open hashing/closed addressing) as an array
+        // of howManyBuckets ArrayLists.
+
+        // Instantiate an array to have an ArrayList as each element of the
+        // array.
+        buckets = new ArrayList[howManyBuckets]; // What about line 9?
+        employeeCount = 0;
+        // For each element in the array, instantiate its ArrayList.
+        for (int i = 0; i < howManyBuckets; i++) {
+            buckets[i] = new ArrayList(); // Instantiate the ArrayList for
+            // bucket i.
+        }
+    }
+
+    // METHODS
+    // Getters and Setters
+    /**
+     * @return the buckets
+     */
+    public ArrayList<EmployeeInfo>[] getBuckets() {
+        return buckets;
+    }
+
+    public int calcBucket(int keyValue) {
+        // Returns the bucket number as the integer keyValue modulo the number
+        // of buckets for the hash table.
+        return (keyValue % buckets.length);
+    }
+
+    public boolean addEmployee(EmployeeInfo theEmployee) {
+        // Add the employee to the hash table. Return true if employee is added
+        // successfully, return false otherwise
+
+        // If the employee number is negative one, it will not be able to add it
+        // to the array.
+        if (theEmployee.getEmpNum() == -1) {
+            return false;
+        }
+
+        // Add the employee to the array based on the calcBucket result
+        buckets[calcBucket(theEmployee.getEmpNum())].add(theEmployee);
+
+        // Increase tally of number of employees
+        employeeCount++;
+
+        if (theEmployee instanceof FullTimeEmployee) {
+            FullTimeEmployee theFTEmployee = (FullTimeEmployee) theEmployee;
+            yearlyPayroll += theFTEmployee.calcGrossAnnualIncome();
+        } else {
+            PartTimeEmployee thePTEmployee = (PartTimeEmployee) theEmployee;
+            yearlyPayroll += thePTEmployee.calcGrossAnnualIncome();
+        }
+
+        // Search for the employee number in the array to confirm it has been
+        // added.
+        if (searchByEmployeeNumber(theEmployee.getEmpNum()) == theEmployee
+                .getEmpNum()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public int searchByEmployeeNumber(int employeeNum) {
+        // Determine the position of the employee in the ArrayList for the
+        // bucket that employee hashes to.
+        // If the employee is not found, return -1.
+        for (int j = 0; j < buckets.length; j++) {
+            for (int k = 0; k < buckets[j].size(); k++) {
+                if (buckets[j].get(k).getEmpNum() != employeeNum) {
+                    continue;
+                } else {
+                    return buckets[j].indexOf(buckets[j].get(k));
+                } // end else
+            } // end for
+        } // end for
+        return -1;
+    } // end method searchByEmployeeNumber
+
+    public EmployeeInfo removeEmployee(int employeeNum) {
+        // Remove the employee from the hash table and return the reference to
+        // that employee. If the employee is not in the hash table, return null.
+        int theBucket = calcBucket(employeeNum);
+        for (int m = 0; m < buckets[theBucket].size(); m++) {
+            if (buckets[theBucket].get(m).getEmpNum() != employeeNum) {
+                continue;
+            } else {
+                EmployeeInfo removed = buckets[theBucket].get(m);
+                if (removed instanceof FullTimeEmployee) {
+                    FullTimeEmployee FTERemoved = (FullTimeEmployee) removed;
+                    yearlyPayroll -= FTERemoved.calcGrossAnnualIncome();
+                }
+                else {
+                    PartTimeEmployee PTERemoved = (PartTimeEmployee) removed;
+                    yearlyPayroll -= PTERemoved.calcGrossAnnualIncome();
+                } 
+                buckets[theBucket].remove(m);
+                // Decrease the tally of number of employees
+                employeeCount--;
+                return removed;
+            }
+        }
+
+        return null;
+
+    }
+
+    public EmployeeInfo getEmployee(int empNum) {
+        // Retrieve an employee's information by searching their employee number
+        int bucketPosition = calcBucket(empNum);
+        int arraylistPosition = searchByEmployeeNumber(empNum);
+        EmployeeInfo theEmp = buckets[bucketPosition].get(arraylistPosition);
+        return theEmp;
+    }
+
+    public void displayContents() {
+        // Print the employee numbers for the employees stored in each bucket's
+        // ArrayList, starting with bucket 0, then bucket 1, and so on.
+
+        for (int i = 0; i < buckets.length; i++) {
+
+            // For the current bucket, print out the emp num for each item in
+            // its ArrayList.
+
+            System.out.println("\nExamining the ArrayList for bucket " + i);
+            int listSize = buckets[i].size();
+            if (listSize == 0) {
+                System.out.println("  Nothing in its ArrayList!");
+            } else {
+                for (int j = 0; j < listSize; j++) {
+                    int theEmpNum = buckets[i].get(j).getEmpNum();
+                    System.out.println("  Employee " + theEmpNum);
+                } // end for
+
+            } // end else
+
+        } // end for
+
+    } // end method displayContents
+
+    public String[][] exportContents() {
+        // Returns an two dimensional array with employee numbers and their information
+        // To be used with the table in the view all jframe
+
+        // A counter to determine the number of rows we need
+        int employeesAdded = 0;
+
+        // Create the two dimensional array to be returned, employee count is the number of employees, 6 is number of infos
+        String[][] allEmployees = new String[employeeCount][6];
+
+        // go through the hashtable
+        for (int i = 0; i < buckets.length; i++) {
+            int listSize = buckets[i].size();
+            if (listSize == 0) {
+            } else {
+                for (int j = 0; j < listSize; j++) {
+                    EmployeeInfo theEmp = buckets[i].get(j);
+                    String sexText;
+                    String workLocText;
+                    switch (theEmp.getSex()) {
+                        case 1:
+                            sexText = "Male";
+                            break;
+                        case 2:
+                            sexText = "Female";
+                            break;
+                        case 3:
+                            sexText = "Other";
+                            break;
+                        case 4:
+                            sexText = "Prefer not to say";
+                            break;
+                        default:
+                            sexText = "Unknown";
+                            break;
+                    }
+                    switch (theEmp.getWorkLoc()) {
+                        case 1:
+                            workLocText = "MUMBAI";
+                            break;
+                        case 2:
+                            workLocText = "DELHI";
+                            break;
+                        case 3:
+                            workLocText = "CHENNAI";
+                            break;
+                        case 4:
+                            workLocText = "BANGLORE";
+                            break;
+                        case 5:
+                            workLocText = "HYDERABAD";
+                            break;
+                        default:
+                            workLocText = "Unknown";
+                            break;
+                    }
+                    allEmployees[employeesAdded][0] = Integer.toString(theEmp.getEmpNum());
+                    allEmployees[employeesAdded][1] = theEmp.getFirstName();
+                    allEmployees[employeesAdded][2] = theEmp.getLastName();
+                    allEmployees[employeesAdded][3] = sexText;
+                    allEmployees[employeesAdded][4] = workLocText;
+                    allEmployees[employeesAdded][5] = Double.toString(theEmp.getDeductRate());
+                    employeesAdded++;
+                } // end for
+
+            } // end else
+
+        } // end for
+        return allEmployees;
+    } // end method exportContents
+
+    public String[][] exportHashTable() {
+        // returns a 2D array of all the data in the hash table
+        // A counter to determine the number of employees we've added so far
+        // int employeesAdded = 0;
+
+        // Create the two dimensional array to be returned, employee count is the number of employees, 11 is number of info types
+        String[][] allEmployees = new String[employeeCount][11];
+        int employeesAdded = 0;
+
+        // Run through the hashtable
+        for (int i = 0; i < buckets.length; i++) {
+            int listSize = buckets[i].size();
+            if (listSize == 0) {
+            } else {
+                for (int j = 0; j < listSize; j++) {
+                    EmployeeInfo theEmp = buckets[i].get(j);
+                    // Conditional data collection depending on employee type. The employee type is the first data point on each row
+                    if (theEmp instanceof FullTimeEmployee) {
+                        FullTimeEmployee theFTEmp = (FullTimeEmployee) theEmp;
+                        allEmployees[employeesAdded][0] = "FTE";
+                        allEmployees[employeesAdded][7] = Double.toString(theFTEmp.getYearlySalary());
+                    } else {
+                        PartTimeEmployee thePTEmp = (PartTimeEmployee) theEmp;
+                        allEmployees[employeesAdded][0] = "PTE";
+                        allEmployees[employeesAdded][8] = Double.toString(thePTEmp.getHourlyWage());
+                        allEmployees[employeesAdded][9] = Double.toString(thePTEmp.getHoursPerWeek());
+                        allEmployees[employeesAdded][10] = Integer.toString(thePTEmp.getWeeksPerYear());
+                    }
+                    allEmployees[employeesAdded][1] = Integer.toString(theEmp.getEmpNum());
+                    allEmployees[employeesAdded][2] = theEmp.getFirstName();
+                    allEmployees[employeesAdded][3] = theEmp.getLastName();
+                    allEmployees[employeesAdded][4] = Integer.toString(theEmp.getSex());
+                    allEmployees[employeesAdded][5] = Integer.toString(theEmp.getWorkLoc());
+                    allEmployees[employeesAdded][6] = Double.toString(theEmp.getDeductRate());
+                    employeesAdded++;
+                } // end for
+
+            } // end else
+
+        } // end for
+        return allEmployees;
+    } // end method exportHashTable
+} // end class MyHashTable
